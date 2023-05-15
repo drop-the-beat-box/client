a463a90cc5b2f301b9f3ffa43eef4df6f3ac8926
CoolWaterMelon <suplife0@khu.ac.kr>
2023-05-15 17:52:43 +0900

    feat : 그리드 레이아웃 적용 및 컬러 수정

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,47 @@
+function Content(props) {
+  return (
+    <div className="content-entity">
+      <div>{props.id}</div>
+      <div>{props.name}</div>
+      <div>{props.date}</div>
+    </div>
+  );
+}
+
 function Body() {
-    return (
-        <body className="body">
-            <div className="body-top">
+  let dataNum = 20;
+  let sampleList = GenerateSampleData(dataNum);
+  let rendering = [];
+  for (let i = 0; i < sampleList.length; i++) {
+    rendering.push(
+      <Content
+        id={sampleList[i].id}
+        name={sampleList[i].name}
+        date={sampleList[i].date}
+      ></Content>
+    );
+  }
 
-            </div>
-            <div className="body-second">
+  return (
+    <body className="body">
+      <div className="body-top"></div>
+      <div className="body-second"></div>
 
-            </div>
+      <div className="body-main">
+        <div className="body-container">{rendering}</div>
+      </div>
+    </body>
+  );
+}
 
-            <div className="body-main">
+function GenerateSampleData(dataNum) {
+  let sampleData = [];
 
-            </div>
-        </body>
-    )
+  for (let index = 0; index < dataNum; index++) {
+    sampleData.push({ id: index, name: ("Entity", index), date: "2020-05-15" });
+  }
+
+  return sampleData;
 }
 
 export default Body;
-
