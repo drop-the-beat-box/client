d871be3d6e0fa0755d1f17266d5d4cf3db69d4dc
SongMinSeok <songms0909@khu.ac.kr>
2023-05-21 12:56:05 +0900

    feat : Upload PopUp Layer Created

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,22 +1,45 @@
+import React, { useState } from "react";
 import SideBarButton from "./SideBarButton";
 
 function SideBar() {
-    
-    return(
-        <aside className="sidebar">
-            <div className="sidebar-top">
-                <SideBarButton name = {"ButtonName"}></SideBarButton>
-            </div>
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-            <div className="sidebar-middle">
-                <div className="sidebar-middleview"></div>
-            </div>
+  const handleButtonClick = () => {
+    setIsPopupOpen(true);
+  };
 
-            <div className="sidebar-bottom">
-            <SideBarButton name = {"TrashCan"}></SideBarButton>
-            </div>
-        </aside>
-    )
+  const handlePopupClose = () => {
+    setIsPopupOpen(false);
+  };
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-top">
+        <SideBarButton
+          name={"ButtonName"}
+          onClick={handleButtonClick}
+        ></SideBarButton>
+      </div>
+
+      <div className="sidebar-middle">
+        <div className="sidebar-middleview"></div>
+      </div>
+
+      <div className="sidebar-bottom">
+        <SideBarButton name={"TrashCan"}></SideBarButton>
+      </div>
+
+      {isPopupOpen && (
+        <div className="popup-overlay">
+          <div className="popup-content">
+            <h1>레이어 팝업</h1>
+            <p>팝업 내용...</p>
+            <button onClick={handlePopupClose}>닫기</button>
+          </div>
+        </div>
+      )}
+    </aside>
+  );
 }
 
 export default SideBar;
--- src/components/SideBarButton.jsx
+++ src/components/SideBarButton.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
-import React from 'react';
+import React from "react";
 
 function SideBarButton(props) {
-    const [isHovering, setIsHovering] = useState(false);
-    const onMouseOver = () => setIsHovering(true);
-    const onMouseOut=() => setIsHovering(false);
+  const [isHovering, setIsHovering] = useState(false);
+  const onMouseOver = () => setIsHovering(true);
+  const onMouseOut = () => setIsHovering(false);
 
-    console.log("Render");
-    return (
-        <button onClick = {(event) => {
-            event.preventDefault();
-            props.onClick();
-        }} 
-        className={isHovering? "sbbutton-hover" : "sbbutton"}
-        onMouseOver={onMouseOver}
-        onMouseOut={onMouseOut}>
-            <div className="sbbutton-text">
-                <p>{props.name}</p>
-            </div>
-        </button>
-    )
+  return (
+    <button
+      onClick={(event) => {
+        event.preventDefault();
+        props.onClick();
+      }}
+      className={isHovering ? "sbbutton-hover" : "sbbutton"}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    >
+      <div className="sbbutton-text">
+        <p>{props.name}</p>
+      </div>
+    </button>
+  );
 }
 
 export default React.memo(SideBarButton);
