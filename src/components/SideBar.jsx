4cebbd4598974af85ec25308f94ddce9f5e97789
SongMinSeok <songms0909@khu.ac.kr>
2023-05-21 15:37:22 +0900

    Feat : 팝업 레이어 내부 파일 선택 버튼 구현
    업로드 동작은 구현하지 않음

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,7 +11,36 @@ function SideBar() {
   const handlePopupClose = () => {
     setIsPopupOpen(false);
   };
+  const [selectedFiles, setSelectedFiles] = useState([]);
 
+  const handleFileChange = (event) => {
+    setSelectedFiles(Array.from(event.target.files));
+  };
+
+  const handleUpload = () => {
+    // 파일 업로드를 수행하는 로직 구현
+    // 예를 들어, 선택된 파일들을 FormData로 랩핑하여 서버에 전송하는 코드를 작성할 수 있음
+    const formData = new FormData();
+    selectedFiles.forEach((file, index) => {
+      formData.append(`file${index}`, file);
+    });
+
+    // AJAX 또는 fetch를 사용하여 서버에 파일을 전송
+    // 예시로 fetch를 사용한 코드를 작성
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 업로드 성공 시에 수행할 작업 처리
+        console.log("파일 업로드 성공:", data);
+      })
+      .catch((error) => {
+        // 업로드 실패 시에 수행할 작업 처리
+        console.error("파일 업로드 실패:", error);
+      });
+  };
   return (
     <aside className="sidebar">
       <div className="sidebar-top">
@@ -32,9 +61,23 @@ function SideBar() {
       {isPopupOpen && (
         <div className="popup-overlay">
           <div className="popup-content">
-            <h1>레이어 팝업</h1>
-            <p>팝업 내용...</p>
-            <button onClick={handlePopupClose}>닫기</button>
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              style={{ marginLeft: "10%" }}
+            />
+            <div className="popup-inner">
+              <div className="popup-inner-content">
+                <div>
+                  {selectedFiles.map((file, index) => (
+                    <p key={index}>{file.name}</p>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <button onClick={handleUpload}>파일 업로드</button>
+            <button onClick={handlePopupClose}>Close</button>
           </div>
         </div>
       )}
