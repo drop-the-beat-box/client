e05f58c3ee10dd32172272e92734feb80007e21d
yxhwxn <officeyh9819@gmail.com>
2023-05-21 21:22:50 +0900

    feat: search bar 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,52 @@
+import { useState } from "react";
 
 function Header() {
-    return (
-        <div className = 'header'>
-            <div className="header-logo">
-                <img src = "https://ar-color-book.s3.ap-northeast-2.amazonaws.com/dropthebeatboxicon.png"
-                alt="logo" width="50px" height = "50px"></img>
-                <h2 className='header-title'>DropTheBeatBox</h2>
-            </div>
+  const [searchInput, setSearchInput] = useState("");
 
-            <form className="header-search">
-                <input name= "searchId" className="header-searchbar" type="search" placeholder="search" height="100"></input>
-                <input className="header-searchbutton" type="submit" value="Go" height="100"></input>
-            </form>
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img
+          src="https://ar-color-book.s3.ap-northeast-2.amazonaws.com/dropthebeatboxicon.png"
+          alt="logo"
+          width="50px"
+          height="50px"
+        ></img>
+        <h2 className="header-title">DropTheBeatBox</h2>
+      </div>
 
-            <div className="header-profile">
-                <h2>Profile</h2>
-            </div>
+      <form className="header-search">
+        <div className="header-searchbar-container">
+          <input
+            name="searchId"
+            className="header-searchbar"
+            type="search"
+            placeholder="search"
+            height="100"
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+              console.log(searchInput);
+            }}
+          ></input>
+          <div
+            className={
+              searchInput ? "header-bottombox" : "header-bottombox-disabled"
+            }
+          ></div>
         </div>
-    )
+        <input
+          className="header-searchbutton"
+          type="submit"
+          value="Go"
+          height="100"
+        ></input>
+      </form>
+
+      <div className="header-profile">
+        <h2>Profile</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
