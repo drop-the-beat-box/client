85a79e68424bcbb9f27d29cf7a4a1382ad57bfd0
SongMinSeok <songms0909@khu.ac.kr>
2023-05-27 12:55:45 +0900

    Feat : 컴포넌트 분리 및 코드 간략화

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,86 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import SideBarButton from "./SideBarButton";
 import Following from "./Following";
 
 function SideBar({ currentPage }) {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [popupType, setPopupType] = useState(null);
-
-  const handleButtonClick = (type) => {
-    setIsPopupOpen(true);
-    setPopupType(type);
-  };
-
-  const handlePopupClose = () => {
-    setIsPopupOpen(false);
-    setPopupType(null);
-  };
-  const [selectedFiles, setSelectedFiles] = useState([]);
-
-  const handleFileChange = (event) => {
-    setSelectedFiles(Array.from(event.target.files));
-  };
-
-  const handleUpload = () => {
-    // 파일 업로드를 수행하는 로직 구현
-    // 예를 들어, 선택된 파일들을 FormData로 랩핑하여 서버에 전송하는 코드를 작성할 수 있음
-    const formData = new FormData();
-    selectedFiles.forEach((file, index) => {
-      formData.append(`file${index}`, file);
-    });
-
-    // AJAX 또는 fetch를 사용하여 서버에 파일을 전송
-    // 예시로 fetch를 사용한 코드를 작성
-    fetch("/upload", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // 업로드 성공 시에 수행할 작업 처리
-        console.log("파일 업로드 성공:", data);
-      })
-      .catch((error) => {
-        // 업로드 실패 시에 수행할 작업 처리
-        console.error("파일 업로드 실패:", error);
-      });
-  };
   let sidebarTopContent = null;
   let sidebarMidContent = null;
   let sidebarBottomContent = null;
-  if (currentPage === "mainpage") {
-    sidebarMidContent = <Following></Following>;
-  } else if (currentPage === "myfilepage") {
-    sidebarTopContent = (
-      <SideBarButton
-        name={"Upload"}
-        onClick={() => handleButtonClick("upload")}
-      ></SideBarButton>
-    );
-    sidebarMidContent = <div className="sidebar-middleview"></div>;
-    sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
-  } else if (currentPage === "sharingfilepage") {
-    sidebarTopContent = <SideBarButton name={"Upload"}></SideBarButton>;
-    sidebarMidContent = <div className="sidebar-middleview"></div>;
-    sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
-  } else if (currentPage === "sharingpage") {
-    sidebarTopContent = (
-      <SideBarButton
-        name={"CreateSharePage"}
-        onClick={() => handleButtonClick("createsharepage")}
-      ></SideBarButton>
-    );
-    sidebarMidContent = <div className="sidebar-middleview"></div>;
-    sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
-  } else if (currentPage === "trashfilepage") {
-    sidebarTopContent = (
-      <SideBarButton
-        name={"Back"}
-        onClick={() => handleButtonClick("back")}
-      ></SideBarButton>
-    );
-    sidebarMidContent = <div className="sidebar-middleview"></div>;
-    sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
+
+  switch (currentPage) {
+    case "mainpage":
+      sidebarMidContent = <Following />;
+      break;
+    case "myfilepage":
+      sidebarTopContent = <SideBarButton name={"Upload"}></SideBarButton>;
+      sidebarMidContent = <div className="sidebar-middleview"></div>;
+      sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
+      break;
+    case "sharingfilepage":
+      sidebarTopContent = <SideBarButton name={"Upload"}></SideBarButton>;
+      sidebarMidContent = <div className="sidebar-middleview"></div>;
+      sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
+      break;
+    case "sharingpage":
+      sidebarTopContent = <SideBarButton name={"CreateSharePage"}></SideBarButton>;
+      sidebarMidContent = <div className="sidebar-middleview"></div>;
+      sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
+      break;
+    case "trashfilepage":
+      sidebarTopContent = <SideBarButton name={"Back"}></SideBarButton>;
+      sidebarMidContent = <div className="sidebar-middleview"></div>;
+      sidebarBottomContent = <SideBarButton name={"TrashCan"}></SideBarButton>;
+      break;
   }
 
   return (
@@ -88,43 +38,7 @@ function SideBar({ currentPage }) {
       <div className="sidebar-top">{sidebarTopContent}</div>
       <div className="sidebar-middle">{sidebarMidContent}</div>
       <div className="sidebar-bottom">{sidebarBottomContent}</div>
-
-      {isPopupOpen && popupType === "upload" && (
-        <div className="popup-overlay-upload">
-          <div className="popup-content-upload">
-            <input
-              type="file"
-              multiple
-              onChange={handleFileChange}
-              style={{ marginLeft: "10%" }}
-            />
-            <div className="popup-inner-upload">
-              <div className="popup-inner-content-upload">
-                <div>
-                  {selectedFiles.map((file, index) => (
-                    <p key={index}>{file.name}</p>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <button onClick={handleUpload}>파일 업로드</button>
-            <button onClick={handlePopupClose}>Close</button>
-          </div>
-        </div>
-      )}
-      {isPopupOpen && popupType === "createsharepage" && (
-        // CreateSharePage 버튼 클릭 시에 팝업 레이아웃 내용
-        <div className="popup-overlay-createshare">
-          <div className="popup-content-createshare"></div>
-        </div>
-      )}
-
-      {isPopupOpen && popupType === "back" && (
-        // Back 버튼 클릭 시에 팝업 레이아웃 내용
-        <div />
-      )}
     </aside>
   );
 }
-
 export default SideBar;
--- src/components/SideBarButton.jsx
+++ src/components/SideBarButton.jsx
@@ -1,25 +1,114 @@
 import { useState } from "react";
 import React from "react";
 
-function SideBarButton(props) {
+function SideBarButton({ name }) {
   const [isHovering, setIsHovering] = useState(false);
   const onMouseOver = () => setIsHovering(true);
   const onMouseOut = () => setIsHovering(false);
 
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [popupType, setPopupType] = useState(null);
+
+  const handleButtonClick = (type) => {
+    setIsPopupOpen(true);
+    setPopupType(type);
+  };
+
+  const handlePopupClose = () => {
+    setIsPopupOpen(false);
+    setPopupType(null);
+  };
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileChange = (event) => {
+    setSelectedFiles(Array.from(event.target.files));
+  };
+
+  const handleUpload = () => {
+    // 파일 업로드를 수행하는 로직 구현
+    // 예를 들어, 선택된 파일들을 FormData로 랩핑하여 서버에 전송하는 코드를 작성할 수 있음
+    const formData = new FormData();
+    selectedFiles.forEach((file, index) => {
+      formData.append(`file${index}`, file);
+    });
+
+    // AJAX 또는 fetch를 사용하여 서버에 파일을 전송
+    // 예시로 fetch를 사용한 코드를 작성
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 업로드 성공 시에 수행할 작업 처리
+        console.log("파일 업로드 성공:", data);
+      })
+      .catch((error) => {
+        // 업로드 실패 시에 수행할 작업 처리
+        console.error("파일 업로드 실패:", error);
+      });
+  };
+
   return (
-    <button
-      onClick={(event) => {
-        event.preventDefault();
-        props.onClick();
-      }}
-      className={isHovering ? "sbbutton-hover" : "sbbutton"}
-      onMouseOver={onMouseOver}
-      onMouseOut={onMouseOut}
-    >
-      <div className="sbbutton-text">
-        <p>{props.name}</p>
-      </div>
-    </button>
+    <>
+      <button
+        onClick={() => handleButtonClick(name)}
+        className={isHovering ? "sbbutton-hover" : "sbbutton"}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}
+      >
+        <div className="sbbutton-text">
+          <p>{name}</p>
+        </div>
+      </button>
+
+      {isPopupOpen && popupType === "Upload" && (
+        <div className="popup-overlay-upload">
+          <div className="popup-content-upload">
+            <button
+              onClick={handlePopupClose}
+              className="close-button"
+            >
+              X
+            </button>
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              style={{ marginLeft: "10%" }}
+            />
+            <div className="popup-inner-upload">
+              <div className="popup-inner-content-upload">
+                <div>
+                  {selectedFiles.map((file, index) => (
+                    <p key={index}>{file.name}</p>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <button onClick={handleUpload}>파일 업로드</button>
+          </div>
+        </div>
+      )}
+      {isPopupOpen && popupType === "CreateSharePage" && (
+        // CreateSharePage 버튼 클릭 시에 팝업 레이아웃 내용
+        <div className="popup-overlay-createshare">
+          <div className="popup-content-createshare">
+            <button
+              onClick={handlePopupClose}
+              className="close-button"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      )}
+
+      {isPopupOpen && popupType === "back" && (
+        // Back 버튼 클릭 시에 팝업 레이아웃 내용
+        <div />
+      )}
+    </>
   );
 }
 
