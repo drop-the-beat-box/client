2e52e165501091579d77bb5e675f1fc0169cf5a6
SongMinSeok <songms0909@khu.ac.kr>
2023-05-28 15:21:48 +0900

    Feat : SharingFilePage '+' icon Added

--- src/components/Following.jsx
+++ src/components/Following.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import circle from "../img/circle.png";
 import { Avatar } from "@material-ui/core";
+import { faCheckCircle, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const persons = [];
 
 for (let i = 1; i <= 5; i++) {
   persons.push({ id: i, image: circle, name: `Following_${i}` });
 }
-function Profile({ image, name }) {
-  return (
-    <div>
-      <div className="profile">
-        <Avatar src={image}></Avatar>
-        <p>{name}</p>
+function Profile({ image, name, page }) {
+  if (page === "MainPage") {
+    return (
+      <div>
+        <div className="profile">
+          <Avatar src={image}></Avatar>
+          <p>{name}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (page === "SharingFilePage") {
+    return (
+      <div>
+        <div className="profile">
+          <Avatar src={image}></Avatar>
+          <p>{name}</p>
+          <FontAwesomeIcon
+            icon={faPlus}
+            className="plus-button"
+            style={{ color: "#415165" }}
+            /*onClick={handlier}*/
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 function Following({ name }) {
@@ -27,6 +46,7 @@ function Following({ name }) {
             key={index}
             image={person.image}
             name={person.name}
+            page={name}
           />
         ))}
       </div>
@@ -39,6 +59,7 @@ function Following({ name }) {
             key={index}
             image={person.image}
             name={person.name}
+            page={name}
           />
         ))}
       </div>
