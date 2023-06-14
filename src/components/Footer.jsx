8494891a1c747e4bdb3b1e675e5a9d12399e8590
송민석 <songms0909@khu.ac.kr>
2023-06-14 18:07:10 +0900

    Design : 카카오 소셜 로그인 버튼 CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 function Footer() {
-    return (
-        <div className="footer">
-            <div className="footer-center">
-                <h4>Contact : suplife0@khu.ac.kr</h4>
-            </div>
-        </div>
-    )
-    
+  return (
+    <div className="footer">
+      <div className="footer-center">
+        <h4>Contact : suplife0@khu.ac.kr</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
